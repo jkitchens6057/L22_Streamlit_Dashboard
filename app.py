361cfb4a3bfa66e6ddc1cbbb660b446f2af64ebc
jkitchens6057 <jkitchens6057@floridapoly.edu>
2026-03-23 00:28:33 +0000

    Consolidated everything into just app.py

--- app.py
+++ app.py
@@ -40,3 +40,19 @@ def compare_crypto(api_url):
         crc[select[1]] = cr2['price']
         st.title("Historical Price Comparison ($)")
         st.line_chart(crc, x="date", y=[select[0],select[1]], y_label="price")
+
+def main():
+    api_url = "https://api.coingecko.com/api/v3/coins/list"
+    id = get_crypto(api_url)
+    api_url = f"https://api.coingecko.com/api/v3/coins/{id}/market_chart?vs_currency=usd&days=365"
+    market = crypto_market_chart(api_url)
+    st.metric('Current Market Price ($)', market['price'].iloc[-1])
+    graph_crypto(market)
+    api_url = "https://api.coingecko.com/api/v3/coins/list"
+    compare_crypto(api_url)
+
+    
+
+
+if __name__ == "__main__":
+    main()
